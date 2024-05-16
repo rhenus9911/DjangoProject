4e32bc0d65039679ae71d8be32109c88871b02c4
rhenus9911 <jeonghoon9911@naver.com>
2024-05-17 00:06:18 +0900

    seventeeth study

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name="main"
+
 urlpatterns = [
     path('', views.main, name="main"),
+    path('<int:question_id>/', views.detail, name="detail"),
+    path('answer/create/<int:question_id>/', views.answer_create, name="answer_create"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from .models import Question
 
 def main(request):
-    return HttpResponse("This is first django Web")
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list':question_list}
+    return render(request, 'main/index.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question':question}
+    return render(request, 'main/detail.html', context)
+
+def answer_create(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
+    return redirect('main:detail', question_id=question.id)
