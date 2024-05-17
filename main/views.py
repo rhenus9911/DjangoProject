4fdf21753e68c6e6611b54b45f3676744472d8d1
rhenus9911 <jeonghoon9911@naver.com>
2024-05-18 01:36:40 +0900

    make question_create

--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Question
+from .forms import QuestionForm
 
 def main(request):
     question_list = Question.objects.order_by('-create_date')
@@ -16,3 +17,16 @@ def answer_create(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
     return redirect('main:detail', question_id=question.id)
+
+def question_create(request):
+    if request.method == "POST":
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('main:main')
+    else:
+         form = QuestionForm()
+    context = {'form':form}
+    return render(request, 'main/question_form.html', context)
